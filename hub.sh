290ed381a7e7c42ef5cb512bbca479ddfcb3015f
Deathrider700 <131989987+Deathrider700@users.noreply.github.com>
2024-10-13 10:32:49 +0530

    Update hub.sh

--- hub.sh
+++ hub.sh
@@ -2,7 +2,7 @@
 
 # Always use bash for setup
 echo "Adding 'thehub' command to your /root/.bashrc..."
-echo "alias thehub='node /path/to/your/script/autopay.js'" >> ~/.bashrc
+echo "alias thehub='node /home/kali/Autohiter/autopay.js'" >> ~/.bashrc
 source ~/.bashrc  # Apply changes to current session
 
 echo "Setup complete. You can now run the 'thehub' command to execute autopay.js."
